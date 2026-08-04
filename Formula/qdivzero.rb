42474c80e0e07d59e7c29e94a0b5d8065413b0b8
José Carlos <jose@valendra.tech>
2026-08-04 14:03:58 +0200

    feat: bump qdivzero formula to v1.0.1

--- Formula/qdivzero.rb
+++ Formula/qdivzero.rb
@@ -1,29 +1,29 @@
 class Qdivzero < Formula
   desc "Command-line client for the QDivZero API"
   homepage "https://github.com/QDivZero/qdivzero-cli"
-  version "1.0.0"
+  version "1.0.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/QDivZero/qdivzero-cli/releases/download/v1.0.0/qdivzero_1.0.0_darwin_arm64.tar.gz"
-      sha256 "40d7285b3fa78de217c1d7c93883a2dbb5b2619e2b20126bf7e501d8ee92aa5c"
+      url "https://github.com/QDivZero/qdivzero-cli/releases/download/v1.0.1/qdivzero_1.0.0_darwin_arm64.tar.gz"
+      sha256 "4fde79ca56a2b56bdaa53fb1530f8ecd9585ffef3c6ed99fa2095cc9f3a46235"
     end
     on_intel do
-      url "https://github.com/QDivZero/qdivzero-cli/releases/download/v1.0.0/qdivzero_1.0.0_darwin_amd64.tar.gz"
-      sha256 "e08b8c819fdd26c089c4d80873687d22c1483b80619fe0ed8b19a8b737f0c9d9"
+      url "https://github.com/QDivZero/qdivzero-cli/releases/download/v1.0.1/qdivzero_1.0.0_darwin_amd64.tar.gz"
+      sha256 "434843dc3b1254f4c0a611b46cdd5ab4567f1718a3a9d237209cf7e8f7c97bc3"
     end
   end
 
   on_linux do
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/QDivZero/qdivzero-cli/releases/download/v1.0.0/qdivzero_1.0.0_linux_arm64.tar.gz"
-        sha256 "4f0480b78d7352ed507c91614afd7b5886815db6da3c88a3ac2922b833f0338f"
+        url "https://github.com/QDivZero/qdivzero-cli/releases/download/v1.0.1/qdivzero_1.0.0_linux_arm64.tar.gz"
+        sha256 "473e7f4dae269c694b04e5d49083e386f0da3c30c5f4b4923f920d6ceec33242"
       end
     end
     on_intel do
-      url "https://github.com/QDivZero/qdivzero-cli/releases/download/v1.0.0/qdivzero_1.0.0_linux_amd64.tar.gz"
-      sha256 "e1d5ccb8d3ac8e5880aa64567bfb5b3ffc7f7b6df7475c231ed7039f08e83c6c"
+      url "https://github.com/QDivZero/qdivzero-cli/releases/download/v1.0.1/qdivzero_1.0.0_linux_amd64.tar.gz"
+      sha256 "c5bcc98de5ea40be10e192bbabb2ca297b85a025a37d39579a186b25fa59d642"
     end
   end
 
